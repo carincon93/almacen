08c0037808e8604e68f7d962d7d56d26e2374660
AndrésQ <anrras@gmail.com>
2017-08-18 14:01:41 -0500

    modal history record

--- resources/views/history_records/index.blade.php
+++ resources/views/history_records/index.blade.php
@@ -1,25 +1,44 @@
 @extends('layouts.app')
 @section('content')
+<div class="modal fade" id="confirm-delete">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title text-capitalize" id="myModalLabel"></h4>
+            </div>
+            <div class="modal-body">
+                Está seguro que desea eliminar este historial?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-danger" id="delete-historical">Eliminar Historial</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="modalFormNovedad">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title text-capitalize" id="myModalLabel"></h4>
+            </div>
+            <div class="modal-body">
+                <form class="" action="index.html" method="post">
+                    asdadas
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-danger" id="delete-historical">Eliminar Historial</button>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="card">
     <div class="table-responsive">
         <table class="table table-full table-hover" data-form="deleteForm">
-            <div class="modal fade" id="confirm-delete">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title text-capitalize" id="myModalLabel"></h4>
-                        </div>
-                        <div class="modal-body">
-                            Está seguro que desea eliminar este historial?
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                            <button type="button" class="btn btn-danger" id="delete-historical">Eliminar Historial</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <thead>
                 <tr>
                     <th>ID</th>
@@ -44,6 +63,7 @@
                     <td>{{ $his->entregado_en != '' ? $his->entregado_en : 'Sin entrega'}}</td>
                     <td>{{ $his->novedad != '' ? $his->novedad : 'Sin novedad'}}</td>
                     <td>
+                        <button type="button" data-target="#modalFormNovedad" data-toggle="modal"></button>
                         <form action="{{ url('/admin/history_record/'.$his->id) }}" method="POST" style="display: inline-block;" class="form-delete-historical btn btn-danger">
                             {{ method_field('delete') }}
                             {!! csrf_field()  !!}
