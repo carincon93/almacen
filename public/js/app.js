515b5e9fb0c11377fb098e03d0bb215e2f0bcd68
carincon93 <carincon93@gmail.com>
2017-10-08 09:07:16 -0500

    modificaciones

--- public/js/app.js
+++ public/js/app.js
@@ -41801,7 +41801,7 @@ var Component = __webpack_require__(37)(
   /* moduleIdentifier (server only) */
   null
 )
-Component.options.__file = "C:\\xampp\\htdocs\\proyectoalmacen2\\resources\\assets\\js\\components\\Example.vue"
+Component.options.__file = "C:\\xampp\\htdocs\\almacen\\resources\\assets\\js\\components\\Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key.substr(0, 2) !== "__"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
@@ -41812,9 +41812,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-1c9aeec8", Component.options)
+    hotAPI.createRecord("data-v-151df636", Component.options)
   } else {
-    hotAPI.reload("data-v-1c9aeec8", Component.options)
+    hotAPI.reload("data-v-151df636", Component.options)
   }
   module.hot.dispose(function (data) {
     disposed = true
@@ -41975,7 +41975,7 @@ module.exports.render._withStripped = true
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-1c9aeec8", module.exports)
+     require("vue-hot-reload-api").rerender("data-v-151df636", module.exports)
   }
 }
 
